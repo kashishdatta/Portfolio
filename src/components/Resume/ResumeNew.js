b59ea678554e0a9ac2b7c263712392d455b3d112
Kashish <89009531+kashishdatta@users.noreply.github.com>
2025-01-27 12:37:53 -0800

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "src/Resume.pdf";
+import pdf from "/vercel/path0/src/components/Resume";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
